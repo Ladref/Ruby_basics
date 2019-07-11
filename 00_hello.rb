af1c55fa3ef1f0c972ae6a8d105bb52f7576977b
ladref <louis.dreyfus@icloud.com>
2019-07-11 09:58:41 +0200

    removing 01 & 02

--- 00_hello.rb
+++ 00_hello.rb
@@ -1,5 +1,12 @@
-def sayHello
-  puts "Bonjour!"
+def sayHello(first_name)
+  puts "Bonjour, #{first_name}!"
 end
 
-sayHello
+def ask_first_name
+puts "What's your name?"
+print "> "
+input = gets.chomp
+return sayHello(input)
+end
+
+ask_first_name
--- 01_hello.rb
+++
@@ -1,5 +0,0 @@
-def sayHello(first_name)
-  puts "Bonjour, #{first_name}!"
-end
-
-sayHello("Louis")
